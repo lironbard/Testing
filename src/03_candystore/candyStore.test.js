a0b4d90622b199ea7388f07032009508c46221e6
lironbard <lironbard@gmail.com>
2022-01-03 22:37:52 +0200

    candyStore:WIP

--- src/03_candystore/candyStore.test.js
+++ src/03_candystore/candyStore.test.js
@@ -3,5 +3,19 @@ import CandyStore from "./candyStore";
 describe("getCandy", () => {
   test("Should return a candy with valid id", () => {
     const candyStore = new CandyStore();
+    const candy = {
+      name: "mint gum",
+      id: "as12f",
+      price: 2,
+      amount: 2,
+    };
+    const queriedCandy = candyStore.getCandy(candy.id);
+    expect(candy).toStrictEqual(queriedCandy);
+  });
+
+  test("Should return undefined if the candy ID doesn't exist", () => {
+    const candyStore = new CandyStore();
+    const queriedCandy = candyStore.getCandy("assdsd");
+    expect(queriedCandy).toBeUndefined();
   });
 });
